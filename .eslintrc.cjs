3ee0de0317c40a1888a3c887e35c4c4c6ba34260
Esraa Jbara <jbesraa@gmail.com>
2020-09-15 14:33:07 +0300

    change eslint file ext

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+	'env': {
+		'es2020': true,
+		'node': true,
+		'jest': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
